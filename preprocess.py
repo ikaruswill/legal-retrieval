2e390e85bae8408ef44987f7da30b122bfaeb86f
Will Ho <ikaruswill.ho@gmail.com>
2017-03-26 21:54:39 +0800

    Implement function to lemmatize tokens

--- preprocess.py
+++ preprocess.py
@@ -13,3 +13,9 @@ def remove_punctuations(docs, key):
 	punctuation = set(string.punctuation)
 	for doc_id, doc in docs.items():
 		docs[doc_id][key] = [token for token in doc[key] if token not in punctuation]
+
+# In-place
+def lemmatize(docs, key):
+	wnl = WordNetLemmatizer()
+	for doc_id, doc in docs.items():
+		docs[doc_id][key] = [wnl.lemmatize(token) for token in doc[key]]
