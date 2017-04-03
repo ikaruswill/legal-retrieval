dc77c453b211048a6fe787623fd1fdf493e2cf29
Chua Chin Siang <ccs6327@gmail.com>
2017-04-04 00:57:15 +0800

    added code for query expansion

--- search.py
+++ search.py
@@ -3,6 +3,7 @@ import sys
 import utility
 import math
 import heapq
+import os
 
 unigram_dict = {}
 bigram_dict = {}
@@ -38,7 +39,7 @@ def get_posting(index, start_offset, postings_offsets):
 	posting = utility.load_object(postings_file)
 	return posting
 
-def strip_and_preproces(line):
+def strip_and_preprocess(line):
 	line = line.strip('" ')
 	line = utility.tokenize(line)
 	line = utility.remove_punctuations(line)
@@ -46,22 +47,24 @@ def strip_and_preproces(line):
 	line = utility.stem(line)
 	return line
 
-def vsm(term, dictionary, posting_offset, start_offset, lengths):
+def vsm(query, dictionary, posting_offset, start_offset, lengths):
 	scores = {}
 	query_weights = []
-	if term in dictionary:
-		# print('term in dict')
-		dict_entry = dictionary.get(term)
-		# print('dict entry', dict_entry)
-		postings_entry = get_posting(dict_entry['index'], start_offset, posting_offset)
-		# print('posting entry', postings_entry)
-		idf = math.log10(len(lengths) / len(postings_entry))
-		for doc_id, doc_tf in postings_entry:
-			doc_tf_weight = 1 + math.log10(doc_tf)
-			if doc_id not in scores:
-				scores[doc_id] = 0
-			scores[doc_id] += doc_tf_weight * idf # query 1 + log(query_tf) always 1
-		query_weights.append(idf)
+	for term, query_tf in query.items():
+		if term in dictionary:
+			# print('term in dict')
+			dict_entry = dictionary.get(term)
+			# print('dict entry', dict_entry)
+			postings_entry = get_posting(dict_entry['index'], start_offset, posting_offset)
+			# print('posting entry', postings_entry)
+			idf = math.log10(len(lengths) / len(postings_entry))
+			query_tf_weight = 1 + math.log10(query_tf)
+			for doc_id, doc_tf in postings_entry:
+				doc_tf_weight = 1 + math.log10(doc_tf)
+				if doc_id not in scores:
+					scores[doc_id] = 0
+				scores[doc_id] += doc_tf_weight * idf * query_tf_weight
+			query_weights.append(idf * query_tf_weight)
 
 	query_l2_norm = math.sqrt(sum([math.pow(query_weight, 2) for query_weight in query_weights]))
 
@@ -76,22 +79,49 @@ def vsm(term, dictionary, posting_offset, start_offset, lengths):
 
 	return [heapq.heappop(scores_heap) for i in range(len(scores_heap))]
 
-def handle_query(query):
-	phrases = list(map(strip_and_preproces, query.split('AND')))
-	result = []
+def process_query_into_ngram(phrase, n):
+	ngrams = utility.generate_ngrams(phrase, n)
+	return utility.count_tokens(ngrams)
+
+def query_with_doc(doc_id):
+	file_path = os.path.join(dir_doc, str(doc_id) + '.xml')
+	if os.path.isfile(file_path):
+		doc_content = utility.extract_doc(file_path).get('content')
+		return handle_query(doc_content, False)
+
+def get_all_doc_ids(results):
+	# just simply OR for now
+	doc_ids = set()
+	for result in results:
+		doc_ids = doc_ids.union(list(map(lambda x: x.doc_id, result)))
+	return doc_ids
+
+def handle_query(query, query_expansion=True):
+	phrases = list(map(strip_and_preprocess, query.split('AND')))
+	print(phrases)
+	results = []
 	for phrase in phrases:
-		term = ' '.join(phrase)
-		print('term:', term)
-		if len(phrase) == 3:
+		if len(phrase) >= 3: #three words original query or documents with more than 3 words
 			print('trigram case')
-			result.append(vsm(term, trigram_dict, trigram_postings_offset, trigram_start_offset, trigram_lengths))
+			processed_query = process_query_into_ngram(phrase, 3)
+			results.append(vsm(processed_query, trigram_dict, trigram_postings_offset, trigram_start_offset, trigram_lengths))
 		elif len(phrase) == 2:
 			print('bigram case')
-			result.append(vsm(term, bigram_dict, bigram_postings_offset, bigram_start_offset, bigram_lengths))
+			processed_query = process_query_into_ngram(phrase, 2)
+			results.append(vsm(processed_query, bigram_dict, bigram_postings_offset, bigram_start_offset, bigram_lengths))
 		else:
 			print('unigram case')
-			result.append(vsm(term, unigram_dict, unigram_postings_offset, unigram_start_offset, unigram_lengths))
-	print('result', result)
+			processed_query = process_query_into_ngram(phrase, 1)
+			results.append(vsm(processed_query, unigram_dict, unigram_postings_offset, unigram_start_offset, unigram_lengths))
+
+	if not query_expansion:
+		return results
+
+	query_expansion_results = []
+	for doc_id in get_all_doc_ids(results):
+		print('query expansion with doc', doc_id)
+		query_expansion_results.append(query_with_doc(doc_id))
+	# TODO do reciprocal with results and query_expansion_results
 
 def main():
 	global unigram_dict, bigram_dict, trigram_dict
@@ -136,19 +166,22 @@ def main():
 				result = handle_query(line)
 
 	postings_file.close()
+	print('completed')
 
 def usage():
-	print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -l lengths-file -o output-file-of-results")
+	print("usage: " + sys.argv[0] + "-i directory-of-documents -d dictionary-file -p postings-file -q file-of-queries -l lengths-file -o output-file-of-results")
 
 if __name__ == '__main__':
 	dict_path = postings_path = query_path = output_path = lengths_path = None
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:l:')
+		opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:q:o:l:')
 	except getopt.GetoptError as err:
 		usage()
 		sys.exit(2)
 	for o, a in opts:
-		if o == '-d':
+		if o == '-i':
+			dir_doc = a
+		elif o == '-d':
 			dict_path = a
 		elif o == '-p':
 			postings_path = a
@@ -160,7 +193,7 @@ if __name__ == '__main__':
 			lengths_path = a
 		else:
 			assert False, "unhandled option"
-	if dict_path == None or postings_path == None or query_path == None or output_path == None or lengths_path == None:
+	if dir_doc == None or dict_path == None or postings_path == None or query_path == None or output_path == None or lengths_path == None:
 		usage()
 		sys.exit(2)
 
