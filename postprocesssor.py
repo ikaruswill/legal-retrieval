67818b527b5ecf31d44a79348990f245c5e72e4f
seokmin <bsmmoon@gmail.com>
2017-04-05 22:29:03 +0800

    improve policies

--- postprocesssor.py
+++ postprocesssor.py
@@ -15,20 +15,22 @@ SAMPLE_SIZE = 20
 
 
 # averaging
-def apply_ranking_policy(processed_record):
+def apply_ranking_policy(processed_record, number_of_queries):
 	if RANKING_POLICY == SUMMATION_POLICY:
 		pass
 	elif RANKING_POLICY == AVERAGING_POLICY:
-		processed_record['score_id_pair'].score /= processed_record['count']
+		processed_record['score_id_pair'].score /= number_of_queries
 	elif RANKING_POLICY == MEAN_RECIPROCAL_RANK_POLICY:
 		processed_record['score_id_pair'].score = processed_record['mrr'] * -1  # bc of heapq
+		processed_record['score_id_pair'].score /= number_of_queries  # will not affect the ranking but by definition
 	else:
 		print("Unknown policy selected. SUMMATION POLICY applied.")
 	return processed_record['score_id_pair']
 
 
 def postprocess(query_expansion_results):
-	print('# of documents queried on query expansion: ', len(query_expansion_results))
+	number_of_queries = len(query_expansion_results)
+	print('# of documents queried on query expansion: ', number_of_queries)
 
 	processed_records = {}
 	for query_expansion_result in query_expansion_results:
@@ -36,14 +38,13 @@ def postprocess(query_expansion_results):
 		for phrasal_query_result in query_expansion_result:
 			for rank, score_doc_id_pair in enumerate(phrasal_query_result):
 				if score_doc_id_pair.doc_id not in processed_records:
-					processed_records[score_doc_id_pair.doc_id] = {'score_id_pair': score_doc_id_pair, 'count': 0, 'mrr': 0}
+					processed_records[score_doc_id_pair.doc_id] = {'score_id_pair': score_doc_id_pair, 'mrr': 0}
 				processed_record = processed_records[score_doc_id_pair.doc_id]
 				processed_record['score_id_pair'].score += score_doc_id_pair.score
-				processed_record['count'] += 1
 				processed_record['mrr'] += (1.0 / (rank + 1))
 
 	for doc_id in processed_records:
-		processed_records[doc_id] = apply_ranking_policy(processed_records[doc_id])
+		processed_records[doc_id] = apply_ranking_policy(processed_records[doc_id], number_of_queries)
 
 	scores_heap = list(processed_records.values())
 	heapq.heapify(scores_heap)
