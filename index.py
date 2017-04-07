aaa2feefe79f1be00f8560b7848d678185a466ac
Will Ho <ikaruswill.ho@gmail.com>
2017-04-07 12:30:10 +0800

    Switch implementation to per document block indexing

--- index.py
+++ index.py
@@ -5,68 +5,15 @@ import utility
 import math
 import pickle
 import logging
+import collections
 
-def load_xml_data(dir_doc):
-	docs = []
-	for dirpath, dirnames, filenames in os.walk(dir_doc):
-		for name in sorted(filenames):
-			if name.endswith('.xml'):
-				file_path = os.path.join(dirpath, name)
-				docs.append(utility.extract_doc(file_path))
+max_block_size = 20 # Number of tokens
 
-	return docs
-
-def preprocess(docs, key):
-	iter_key_call(docs, key, utility.tokenize)
-	iter_key_call(docs, key, utility.remove_punctuations)
-	iter_key_call(docs, key, utility.remove_stopwords)
-	iter_key_call(docs, key, utility.stem)
-
-def build_dictionary(docs, key):
-	terms = set()
-	for doc in docs:
-		terms.update(doc[key].keys())
-
-	sorted_terms = sorted(list(terms))
-
-	dictionary = {}
-	for i, term in enumerate(sorted_terms):
-		dictionary[term] = {'index': i}
-
-	return dictionary
-
-# def build_inverted_dictionary(dictionary):
-# 	return [term for term, item in sorted(dictionary.items())]
-
-def build_and_populate_lengths(docs, key):
-	lengths = {}
-	for doc in docs:
-		sum_squares = 0
-		for term, freq in doc[key].items():
-			sum_squares += math.pow(1 + math.log10(freq), 2)
-		doc_id = doc['document_id']
-		lengths[doc_id] = math.sqrt(sum_squares)
-
-	return lengths
-
-# Also modifies dictionary by adding doc_freq key for each term
-def build_and_populate_postings(docs, key, dictionary):
-	postings = []
-	for term in dictionary:
-		postings.append([])
-
-	for doc in docs:
-		doc_id = int(doc['document_id'])
-		for term, freq in doc[key].items():
-			index = dictionary[term]['index']
-			# if len(postings[index]) > 0:
-			# 	gap = doc_id - postings[index][-1][0]
-			# 	postings[index].append((gap, freq))
-			# else:
-			# 	postings[index].append((doc_id, freq))
-			postings[index].append((doc_id, freq))
-
-	return postings
+def get_length(counted_tokens):
+	sum_squares = 0
+	for term, freq in counted_tokens.items():
+		sum_squares += math.pow(1 + math.log10(freq), 2)
+	return math.sqrt(sum_squares)
 
 def save_postings(postings, f):
 	sizes = []
@@ -83,62 +30,91 @@ def save_postings(postings, f):
 	for serialized_posting in serialized_postings:
 		f.write(serialized_posting)
 
-def iter_key_call(iterable, key, function, *args, **kwargs):
-	for dict_item in iterable:
-		dict_item[key] = function(dict_item[key], *args, **kwargs)
-
-def copy_key(dicts, src_key, dest_key):
-	for item in dicts:
-		item[dest_key] = item[src_key]
-
-def delete_key(dicts, delete_key):
-	for item in dicts:
-		item.pop(delete_key)
+def prepare_filepath(path, block_number):
+	temp_folder = 'tmp/'
+	path, name = os.path.split(path)
+	name, ext = os.path.splitext(name)
+	path = os.path.join(path, temp_folder)
+	if not os.path.exists(path):
+		os.makedirs(path)
+	return os.path.join(path, name + '_' + str(block_number))
 
 def usage():
 	print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file -l lengths-file")
 
 def main():
-	logging.info('Creating new files')
-	# Append binary mode for repeated pickling and creation of new file
-	dict_file = open(dict_path, 'ab+')
-	lengths_file = open(lengths_path, 'ab+')
-	postings_file = open(postings_path, 'ab+')
-
-	logging.info('Parsing XML')
 	content_key = 'content'
-	docs = load_xml_data(dir_doc)
-	logging.info('Preprocessing documents')
-	preprocess(docs, content_key)
+	key = 'unigram'
+	log_every_n = 10
+	for dirpath, dirnames, filenames in os.walk(dir_doc):
+		filename_queue = collections.deque(sorted(filenames)) # Sorted by DocID
+		processed_count = 0
+		iteration_count = 0
+		block_size = 0
+		while len(filename_queue):
+			dictionary_set = set()
+			postings_dict = {}
+			lengths = {}
+			while block_size < max_block_size and len(filename_queue):
+				filename = filename_queue.popleft()
+				if not filename.endswith('xml'):
+					break
+				file_path = os.path.join(dirpath, filename)
+				doc = utility.extract_doc(file_path)
+				logging.info('Tokenizing document')
+				doc[content_key] = utility.tokenize(doc[content_key])
+				logging.info('Removing punctuations')
+				doc[content_key] = utility.remove_punctuations(doc[content_key])
+				logging.info('Removing stopwords')
+				doc[content_key] = utility.remove_stopwords(doc[content_key])
+				logging.info('Stemming tokens')
+				doc[content_key] = utility.stem(doc[content_key])
+				logging.info('Generating ngrams')
+				doc[content_key] = utility.generate_ngrams(doc[content_key], 1)
+				logging.info('Counting terms')
+				doc[content_key] = utility.count_tokens(doc[content_key])
+				logging.info('Processing document')
+				for term, freq in doc[content_key].items():
+					if term not in dictionary_set:
+						dictionary_set.add(term)
+						postings_dict[term] = []
+					postings_dict[term].append((doc['document_id'], freq))
+					lengths[doc['document_id']] = get_length(doc[content_key])
+				block_size += 1
+				processed_count += 1
 
-	keys = ['unigram', 'bigram', 'trigram']
+				if processed_count % log_every_n == 0:
+					logging.info('-- Processed %s', processed_count)
 
-	for i, key in enumerate(keys):
-		ngram = i + 1
-		logging.info('[%s] Copying key', key)
-		copy_key(docs, content_key, key)
-		logging.info('[%s] Generating ngrams', key)
-		iter_key_call(docs, key, utility.generate_ngrams, n=ngram)
-		logging.info('[%s] Counting tokens', key)
-		iter_key_call(docs, key, utility.count_tokens)
-		logging.info('[%s] Building & populating lengths', key)
-		lengths = build_and_populate_lengths(docs, key)
-		logging.info('[%s] Building dictionary', key)
-		dictionary = build_dictionary(docs, key)
-		logging.info('[%s] Building & populating postings', key)
-		postings = build_and_populate_postings(docs, key, dictionary)
-		logging.info('[%s] Saving postings', key)
-		save_postings(postings, postings_file)
-		logging.info('[%s] Saving dictionary and lengths', key)
-		utility.save_object(dictionary, dict_file)
-		utility.save_object(lengths, lengths_file)
-		delete_key(docs, key)
+			# Prepare dictionary and postings
+			sorted_terms = sorted(list(dictionary_set))
+			dictionary = {}
+			postings = []
+			for i, term in enumerate(sorted_terms):
+				dictionary[term] = {'index': i, 'doc_freq': len(postings_dict[term])}
+				postings.append(postings_dict[term]) # Append list reference
 
-	logging.info('Closing files')
-	dict_file.close()
-	lengths_file.close()
-	postings_file.close()
-	logging.info('Indexing complete')
+			# Save block
+			# NEED TO HANDLE RELATIVE PATHS AND LACK OF TRAILING SLASHES
+			block_dict_path = prepare_filepath(dict_path, iteration_count)
+			block_lengths_path = prepare_filepath(lengths_path, iteration_count)
+			block_postings_path = prepare_filepath(postings_path, iteration_count)
+
+			dict_file = open(block_dict_path, 'wb')
+			lengths_file = open(block_lengths_path, 'wb')
+			postings_file = open(block_postings_path, 'wb')
+
+			utility.save_object(dictionary, dict_file)
+			utility.save_object(lengths, lengths_file)
+			utility.save_object(postings_dict, postings_file)
+
+			dict_file.close()
+			lengths_file.close()
+			postings_file.close()
+
+			block_size = 0
+			iteration_count += 1
+			print('--- Iteration Count: ', iteration_count)
 
 if __name__ == '__main__':
 	logging.basicConfig(level=logging.INFO, datefmt='%d/%m/%y %H:%M:%S', format='%(asctime)s %(message)s')
