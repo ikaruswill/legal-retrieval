fd122aa3a2860675a9e177ddeebcab2086e8cb3f
Will Ho <ikaruswill.ho@gmail.com>
2017-04-07 13:22:04 +0800

    Export index dictionary instead of separating into dict and postings

--- index.py
+++ index.py
@@ -7,7 +7,10 @@ import pickle
 import logging
 import collections
 
-max_block_size = 20 # Number of tokens
+max_block_size = 20 # Number of documents
+script_path = os.path.dirname(os.path.realpath(__file__))
+log_every_n = 10
+content_key = 'content'
 
 def get_length(counted_tokens):
 	sum_squares = 0
@@ -30,31 +33,25 @@ def save_postings(postings, f):
 	for serialized_posting in serialized_postings:
 		f.write(serialized_posting)
 
-def prepare_filepath(path, block_number):
+def prepare_filepath(script_path, block_number, suffix):
 	temp_folder = 'tmp/'
-	path, name = os.path.split(path)
-	name, ext = os.path.splitext(name)
-	path = os.path.join(path, temp_folder)
-	if not os.path.exists(path):
-		os.makedirs(path)
-	return os.path.join(path, name + '_' + str(block_number))
+	temp_path = os.path.join(script_path, temp_folder)
+	if not os.path.exists(temp_path):
+		os.makedirs(temp_path)
+	return os.path.join(temp_path, '_'.join((str(block_number), suffix)))
 
 def usage():
 	print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file -l lengths-file")
 
 def main():
-	content_key = 'content'
-	key = 'unigram'
-	log_every_n = 10
 	for dirpath, dirnames, filenames in os.walk(dir_doc):
 		filename_queue = collections.deque(sorted(filenames)) # Sorted by DocID
 		processed_count = 0
 		iteration_count = 0
 		block_size = 0
 		while len(filename_queue):
-			dictionary_set = set()
-			postings_dict = {}
-			lengths = {}
+			block_index = {}
+			block_lengths = {}
 			while block_size < max_block_size and len(filename_queue):
 				filename = filename_queue.popleft()
 				if not filename.endswith('xml'):
@@ -75,46 +72,36 @@ def main():
 				doc[content_key] = utility.count_tokens(doc[content_key])
 				logging.info('Processing document')
 				for term, freq in doc[content_key].items():
-					if term not in dictionary_set:
-						dictionary_set.add(term)
-						postings_dict[term] = []
-					postings_dict[term].append((doc['document_id'], freq))
-					lengths[doc['document_id']] = get_length(doc[content_key])
+					if term not in block_index:
+						block_index[term] = []
+					block_index[term].append((doc['document_id'], freq))
+					block_lengths[doc['document_id']] = get_length(doc[content_key])
 				block_size += 1
 				processed_count += 1
 
 				if processed_count % log_every_n == 0:
-					logging.info('-- Processed %s', processed_count)
-
-			# Prepare dictionary and postings
-			sorted_terms = sorted(list(dictionary_set))
-			dictionary = {}
-			postings = []
-			for i, term in enumerate(sorted_terms):
-				dictionary[term] = {'index': i, 'doc_freq': len(postings_dict[term])}
-				postings.append(postings_dict[term]) # Append list reference
+					print('- Processed', processed_count)
 
 			# Save block
 			# NEED TO HANDLE RELATIVE PATHS AND LACK OF TRAILING SLASHES
-			block_dict_path = prepare_filepath(dict_path, iteration_count)
-			block_lengths_path = prepare_filepath(lengths_path, iteration_count)
-			block_postings_path = prepare_filepath(postings_path, iteration_count)
+			block_index_path = prepare_filepath(script_path, suffix='index', block_number=iteration_count)
+			block_lengths_path = prepare_filepath(script_path, suffix='lengths', block_number=iteration_count)
 
-			dict_file = open(block_dict_path, 'wb')
-			lengths_file = open(block_lengths_path, 'wb')
-			postings_file = open(block_postings_path, 'wb')
+			block_index_file = open(block_index_path, 'wb')
+			block_lengths_file = open(block_lengths_path, 'wb')
 
-			utility.save_object(dictionary, dict_file)
-			utility.save_object(lengths, lengths_file)
-			utility.save_object(postings_dict, postings_file)
+			utility.save_object(block_index, block_index_file)
+			utility.save_object(block_lengths, block_lengths_file)
 
-			dict_file.close()
-			lengths_file.close()
-			postings_file.close()
+			block_index_file.close()
+			block_lengths_file.close()
 
 			block_size = 0
 			iteration_count += 1
-			print('--- Iteration Count: ', iteration_count)
+			print('--- Iteration Count:', iteration_count)
+
+	# Merge step
+
 
 if __name__ == '__main__':
 	logging.basicConfig(level=logging.INFO, datefmt='%d/%m/%y %H:%M:%S', format='%(asctime)s %(message)s')
