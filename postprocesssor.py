9e3a2bd373c2e508f2ef6c89721ac4759598e42c
seokmin <bsmmoon@gmail.com>
2017-04-08 21:03:16 +0800

    boolean query handling implemented

--- postprocesssor.py
+++ postprocesssor.py
@@ -9,18 +9,19 @@ POST_PROCESSOR_DIR = './query_exp_results.txt'
 AVERAGING_POLICY = 0
 SUMMATION_POLICY = 1
 MEAN_RECIPROCAL_RANK_POLICY = 2
-RANKING_POLICY = SUMMATION_POLICY
+QUERY_EXPANSION_RANKING_POLICY = SUMMATION_POLICY
+BOOLEAN_QUERY_RANKING_POLICY = MEAN_RECIPROCAL_RANK_POLICY
 
 SAMPLE_SIZE = 20
 
 
 # averaging
-def apply_ranking_policy(processed_record, number_of_queries):
-	if RANKING_POLICY == SUMMATION_POLICY:
+def apply_ranking_policy(processed_record, number_of_queries, policy):
+	if policy == SUMMATION_POLICY:
 		pass
-	elif RANKING_POLICY == AVERAGING_POLICY:
+	elif policy == AVERAGING_POLICY:
 		processed_record['score_id_pair'].score /= processed_record['count']
-	elif RANKING_POLICY == MEAN_RECIPROCAL_RANK_POLICY:
+	elif policy == MEAN_RECIPROCAL_RANK_POLICY:
 		processed_record['score_id_pair'].score = processed_record['mrr'] * -1  # bc of heapq
 		processed_record['score_id_pair'].score /= number_of_queries  # will not affect the ranking but by definition
 	else:
@@ -28,37 +29,44 @@ def apply_ranking_policy(processed_record, number_of_queries):
 	return processed_record['score_id_pair']
 
 
-def postprocess(query_expansion_results):
-	number_of_queries = len(query_expansion_results)
-	print('# of documents queried on query expansion: ', number_of_queries)
-
+def combine_rankings(rankings, policy):
+	number_of_queries = len(rankings)
+	print('# of rankings combined: ', number_of_queries)
 	processed_records = {}
-	for query_expansion_result in query_expansion_results:
-		for document_query_result in query_expansion_result:
-			for rank, score_doc_id_pair in enumerate(document_query_result):
-				if score_doc_id_pair.doc_id not in processed_records:
-					processed_records[score_doc_id_pair.doc_id] = {'score_id_pair': score_doc_id_pair, 'count': 0,
-																   'mrr': 0}
-				processed_record = processed_records[score_doc_id_pair.doc_id]
-				processed_record['score_id_pair'].score += score_doc_id_pair.score
-				processed_record['count'] += 1
-				processed_record['mrr'] += (1.0 / (rank + 1))
-
+	for ranking in rankings:
+		for rank, score_doc_id_pair in enumerate(ranking):
+			if score_doc_id_pair.doc_id not in processed_records:
+				processed_records[score_doc_id_pair.doc_id] = {'score_id_pair': score_doc_id_pair, 'count': 0,
+															   'mrr': 0}
+			processed_record = processed_records[score_doc_id_pair.doc_id]
+			processed_record['score_id_pair'].score += score_doc_id_pair.score
+			processed_record['count'] += 1
+			processed_record['mrr'] += (1.0 / (rank + 1))
+			processed_records[score_doc_id_pair.doc_id] = processed_record
 	for doc_id in processed_records:
-		processed_records[doc_id] = apply_ranking_policy(processed_records[doc_id], number_of_queries)
+		processed_records[doc_id] = apply_ranking_policy(processed_records[doc_id], number_of_queries, policy)
 
 	scores_heap = list(processed_records.values())
 	heapq.heapify(scores_heap)
 	result = [heapq.heappop(scores_heap) for _ in range(len(scores_heap))]
+
 	return result
+
+
+def postprocess(boolean_query_results):
+	phrasal_query_rankings = []
+	for query_expansion_result in boolean_query_results:
+		phrasal_query_rankings.append(combine_rankings(query_expansion_result, QUERY_EXPANSION_RANKING_POLICY))
+	combined_ranking = combine_rankings(phrasal_query_rankings, BOOLEAN_QUERY_RANKING_POLICY)
+	return combined_ranking
 
 
 def main():
 	f = open(POST_PROCESSOR_DIR, 'rb')
-	query_expansion_results = utility.load_object(f)
+	boolean_query_results = utility.load_object(f)
 	f.close()
 
-	result = postprocess(query_expansion_results)
+	result = postprocess(boolean_query_results)
 	print('Result size: ', len(result))
 	print(list(map(lambda x: x.doc_id, result[:SAMPLE_SIZE])))
 
